4b6f62981235a443067c0ed68a728b2d9555a6c5
eklopfstein <43417850+eklopfstein@users.noreply.github.com>
2019-04-07 03:01:07 -0400

    Update next.js

--- commands/next.js
+++ commands/next.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (power = "off") {
-        return;
-    }
     else {
         message.channel.send(botconfig.nextMeeting);
     }
