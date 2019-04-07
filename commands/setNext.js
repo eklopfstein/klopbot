e47a0ed363e9637bda768e2f9d49b208f965d366
eklopfstein <43417850+eklopfstein@users.noreply.github.com>
2019-04-07 03:02:34 -0400

    Update setNext.js

--- commands/setNext.js
+++ commands/setNext.js
@@ -1,10 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("ADMINISTATOR")) {
-        return message.reply("Only admins can use this command.");
-    }
-    else {
         botconfig.nextMeeting = args.join(" ").slice(0);
         message.channel.send(nextMeeting);
         bot.guilds.get('372585004871712779').channels.get('#398235324532129814').send(botconfig.nextMeeting);
