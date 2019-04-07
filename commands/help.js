a8ad94ae2dff2ac2cf7a5016da2c8d294dc325f5
eklopfstein <43417850+eklopfstein@users.noreply.github.com>
2019-04-07 03:03:21 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,12 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     {
-        else {
             message.channel.send("Commands:\n" +
                 "!next                          Show date of next meeting(will be posted in Announcements chat) \n" +
                 "!what                         Tells you what this bot is and why it's here\n" +
                 "!helpAdm                Show admin commands(admin only)");
-        }
     }
 }
 
