f46a338b515a4702191401d2f36e23457a821b72
eklopfstein <43417850+eklopfstein@users.noreply.github.com>
2019-04-07 03:02:53 -0400

    Update canRob.js

--- commands/canRob.js
+++ commands/canRob.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     {
-        else if (!message.member.hasPermission("ADMINISTATOR")) {
+        if (!message.member.hasPermission("ADMINISTATOR")) {
             return message.reply("Only admins can use this command.");
         }
         else {
