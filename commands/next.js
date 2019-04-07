75a975ff596b6b40a035d71cd9027aead4d3686a
eklopfstein <43417850+eklopfstein@users.noreply.github.com>
2019-04-07 03:04:17 -0400

    Update next.js

--- commands/next.js
+++ commands/next.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    else {
         message.channel.send(botconfig.nextMeeting);
-    }
 }
 module.exports.help = {
     name: "next"
