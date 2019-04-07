cba88c87309421cf5fc0cc4ff2d2d7283c1b7b87
Elijah Klopfstein <elijahklopfstein@outlook.com>
2019-04-07 02:31:42 -0400

    commands

---
+++ commands/canRob.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    {
+        if (power = "off") {
+            return;
+        }
+        else if (!message.member.hasPermission("ADMINISTATOR")) {
+            return message.reply("Only admins can use this command.");
+        }
+        else {
+            bot.guilds.get('372585004871712779').channels.get('#398235324532129814').send("The upcoming robotics meeting has been cancelled.");
+        }
+    }
+}
+
+module.exports.help = {
+    name: "canRob"
+}
---
+++ commands/next.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if (power = "off") {
+        return;
+    }
+    else {
+        message.channel.send(botconfig.nextMeeting);
+    }
+}
+module.exports.help = {
+    name: "next"
+}
