33dedcd0b5cba92abfe6378b5b6be6ea2b4c7236
eklopfstein <elijahklopfstein@outlook.com>
2020-01-18 15:34:34 -0500

    Allow commands through DMs
    Admin commands not yet included.

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ bot.on("ready", async () => {
 
 bot.on("message", async message => {
     if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
 
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
